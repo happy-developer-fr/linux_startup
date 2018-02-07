a51d01ee18e3c19bc4e15cd5c98b58c35e5879e9
julien.cottet <julien.cottet@group.leguide.com>
2018-02-07 15:25:04 +0100

    init

---
+++ tools.sh
@@ -0,0 +1,8 @@
+#!bin/sh
+sudo apt-get install libc++1
+sudo apt-get install libcurl3
+sudo apt-get install locate
+sudo apt-get install htop
+sudo apt-get install tree
+sudo apt-get install gconf-editor
+
